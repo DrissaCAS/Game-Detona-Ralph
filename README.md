dc58653dbaecf3ce7489e398177f5f5aad236ba1
DRISSA CAS <drissa_cas@hotmail.com>
2024-11-17 17:45:45 -0300

    Finishing the Project challenge.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Game-Detona-Ralph
+# Game Detona Ralph
+Desafio do [Bootcamp da RiHappy na Dio.me](https://web.dio.me/track/f31d0ec6-b46a-47d6-b5ec-5dcd3766dacb).
+
+Nesse desafio pude desenvolver habilidades do HTML, CSS e JavaScript. Nesse desafio utilizei um pouco dos conceitos de HTML semântica e organização de código e arquivos.
+Separação de variaveis de acordo ao que cada variavel faz, se é algo interno ou visual. Fazer sempre funções bem definidas. Trabalhar com eventos e áudios.
+
+Este foi o resultado final.
+![Resultado final](image.png)
--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,17 +5,70 @@ const state = {
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"),
     },
-    value:{},
+    value:{
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        curretTime: 10,
+    },
+    actions:{
+        timeId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
 };
+
+function countDown(){
+    state.value.curretTime--;
+    state.view.timeLeft.textContent = state.value.curretTime;
+    
+    if(state.value.curretTime <= 0){
+        alert("Game Over! O seu resultado foi: " + state.value.result);
+        
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timeId);
+        clearInterval(state.value.result);
+    }
+    
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy")
+
+    state.value.hitPosition = randomSquare.id;
+}
+
+// function moveEnemy(){
+//     state.value.timeId = setInterval(randomSquare, state.value.gameVelocity);
+// }
 
 function addListenerHitBox(){
     state.view.squares.forEach((square) => {
-        
-    })
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.value.hitPosition){
+                state.value.result++;
+                state.view.score.textContent = state.value.result;
+                state.value.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
 }
 
 function init(){
-
+    // moveEnemy();
+    addListenerHitBox();
 }
 
 init();
